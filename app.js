15ec49a194aae45bf1f2b3e77348234109939c79
Dan Stevens <dan.stevens@oracle.com>
2018-10-25 17:17:45 +0100

    Various changes after following Heroku deployment guide for Node (https://devcenter.heroku.com/articles/deploying-nodejs)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function(request, response) {
   response.send("<h1>Hello, World!</h1>");
@@ -9,10 +10,10 @@ app.get("/users/:name", function(req, res) {
   res.send(`<h1>Hello, ${req.params["name"]}</h1>`);
 });
 
-app.listen(80, function(error) {
+app.listen(PORT, function(error) {
   if (error) {
     console.log("An error occurred: " + error);
   } else {
-    console.log("Listening on localhost:3000");
+    console.log("express started");
   }
 });
